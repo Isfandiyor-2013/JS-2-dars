009980676aa1862bee43300a28b4f310d25a05a5
shokirjonovasaodat <abdukarimovisfandiyor9@gmail.com>
2025-02-13 10:27:08 +0500

    JS 2-dars
    Zeromax IT-center 40-guruh

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// if           => aqar
+// else         => u holda
+// else if      => yoki bo'lmasa
+
+// if("salom" == "salom"){
+//     console.log("Ular teng!");
+// }
+// else{
+//     console.log("Ular teng emas");
+// }
+
+// if(45 >= 45){
+//     console.log("Berilgan qiymatlar teng yoki birinchisi katta!");
+// }
+// else{
+//     console.log("Berilgan qiymatlar teng emas yoki birinchisi katta emas")
+// }
+
+// if(45 <= 46){
+//     console.log("Solishtirilayotgan son 45ga teng emas yoki kichik")
+// }
+// else{
+//     console.log("Solishtirilayotgan son 45ga teng yoki katta")
+// }
+
+// if("salom men maktabni yahshi ko'raman" == "salom men maktabni yahshi ko'raman"){
+//     console.log("Bu so'zlar teng")
+// }
+// else{
+//     console.log("Bu so'zlar teng emas")
+// }
+// 
+// if(67*9999/9+89-39 >= 67*45-765/21+22756){
+//     console.log("Bu sonlar qiymati teng yoki birinchisi katta!")
+// }
+// else{
+//     console.log("Bu sonlar qiymati teng emas yoki birinchisi katta emas!")
+// }
+// 
+// if(30+12-5+3*1/13 <= 30*99/1+539-845){
+//     console.log("Berilgan sonlar qiymati teng yoki birinchisi kichik!")
+// }
+// else{
+//     console.log("Berilgan sonlar qiymati teng yoki birinchisi katta!")
+// }
+
+// if(45 == 45){
+//     console.log("1-shart bajarildi!")
+// }
+// else if(45 == 45){
+//     console.log("2-shart bajarildi!")
+// }
+// else{
+//     console.log("Yuqoridagi shartlardan birortasi bajarilmadi!")
+// }
+
+// if("Mening ismim isfandiyor" == "Mening ismim isfandiyor"){
+//     console.log("Ona tili o'tdik!")
+// }
+// else if("Mening ismim isfandiyor" == "Mening ismim isfandiyor"){
+//     console.log("Matematika o'tdik!")
+// }
+// else{
+//     console.log("Yuqoridagi fanlardan birortasi ham bo'lmadi!")
+// }
+ 
+// if(90 == 90){
+//     console.log("1-etajga chiqdik!")
+// }
+// else if(90 == 90){
+//    console.log("2-etajga chiqdik!")
+// }
+// else{
+//     console.log("Yuqoridagi etajlardan birortasiga ham chiqmadik!")
+// }
+
+// if(45 == "45"){
+//     console.log("Ular teng!");
+// }
+// else{
+//     console.log("Ular teng emas!");
+// }
+
+// let a = 45;
+// if(a >= 45){
+//     console.log("a berilgan sondan kichik yoki teng")
+// }
+// else{
+//     console.log("a ning qiymati topilmadi")
+// }
+
+let a = 46;
+let b = 10;
+let c = 19;
+let d = 49;
+
+// if(a == 46 && b == 10 && c == 19 && d == 49){
+//     console.log("Shartlarning hammasi to'g'ri!")
+// }
+// else{
+//     console.log("Shartlarning kamida bittasi noto'g'ri!")
+// }
+
+if(a == 45 || b == 10 || c == 19 || d == 49){
+    console.log("Shartlarning kamida bittasi to'g'ri")
+}
+else{
+    console.log("Shartlarning hammasi noto'g'ri")
+}
